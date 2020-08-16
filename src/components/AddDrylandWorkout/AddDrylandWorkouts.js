a19a45f194643ac1c1154ad85377362db5adb25c
HelenHMaher <helenharlanmaher@gmail.com>
2020-08-16 18:48:26 +0200

    global styles

---
+++ src/components/AddDrylandWorkout/AddDrylandWorkouts.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledAddDrylandWorkouts } from "./AddDrylandWorkouts.styled";
+
+function AddDrylandWorkouts() {
+  return (
+    <StyledAddDrylandWorkouts>AddDrylandWorkouts</StyledAddDrylandWorkouts>
+  );
+}
+
+export default AddDrylandWorkouts;
